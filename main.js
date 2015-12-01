6292c3f62c2478c843667e1af4cc3128ff9f18e7
jsdario <jesus@netbeast.co>
2015-12-01 10:09:44 +0100

    adds static folder in case of change wanted

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
 #!/usr/bin/env node
 
-var tty = require('tty.js');
-var argv = require('minimist')(process.argv.slice(2)); // must-have package
+var tty = require('tty.js')
+var argv = require('minimist')(process.argv.slice(2)) // must-have package
 
-/* xyos apps need to accept the port to be launched by parameters */
-port = argv.port;
-
-if(isNaN(port)) {
-	console.log("Port \"" + port + "\" is not a number.");
-	process.kill(1);
-} else {
-	console.log("Listening on port " + port);
-}
-
+// Netbeast apps need to accept the port to be launched by parameters
+var port = argv.port
 
 var app = tty.createServer({
   shell: 'bash',
-  port: port
-});
+  port: port || 31416,
+  static: './assets'
+})
 
-app.listen();
+app.listen()
